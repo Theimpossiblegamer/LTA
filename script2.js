dad11f3893665d465549e8d5d0dffcb754d6c2ee
Boboulder101 <ture@DAT-1601.local>
2017-02-02 13:53:13 +0200

    kek

--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
-	$("button").click(function(){
-		$.get("text.txt","",function(data, status){
-		},"text");
-	});
+$(document).ready(function () {
+	alert("Ready!");
+	$("#div1").load("cfg.txt #date");
+	//$(".safedays").load("cfg.txt #date");
+	$("#nracc").load("cfg.txt #nrAccidents");
+	$("#lti").load("cfg.txt #lostTime");
+	$("#daylti").load("cfg.txt #days");
+
 });
